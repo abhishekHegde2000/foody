925fd84ebb6eaa26c35aa2356b62b6323b621a03
abhishek Hegde <ahegde2021@gmail.com>
2023-10-30 22:49:08 +0530

    commit - 3 - map driven data ui.

--- src/Components/ResCard.jsx
+++ src/Components/ResCard.jsx
@@ -5,24 +5,22 @@ import {basePath} from '../utils/constants'
 
 
 const ResCard = ({resData}) => {
-    console.log(resData)
 
 
     // Destructure the properties from resData
-    const { name, locality, avgRating, costForTwo, cloudinaryImageId } = resData.info;
+    const { name, locality, avgRating, costForTwo, cloudinaryImageId , id } = resData?.info;
   
-
+    
 
     
   return (
     <div className=' border m-2 p-2 border-black inline-block'>
-        <img className= "w-40 h-30" src={basePath + cloudinaryImageId} alt="masala dosa" />
+        <img className= "w-40 h-30" src={basePath + cloudinaryImageId} alt="image not available" />
         <div>
             <h1>{name}</h1>
             <p>{locality}</p>
             <p>{avgRating} stars</p>
             <p>{costForTwo}</p>
-            
         </div>
     </div>
   )
--- src/Components/ResContainer.jsx
+++ src/Components/ResContainer.jsx
@@ -6,20 +6,32 @@ import resObj from '../utils/swiggyData'
 
 const resData = resObj?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
 
-
+console.log(resData)
 const ResContainer = () => {
+//   return (
+//     <div className='flex flex-wrap justify-between'>
+
+//         <ResCard resData={resData[5]}/>
+//         <ResCard resData={resData[1]}/>
+//         <ResCard resData={resData[0]}/>
+//         <ResCard resData={resData[4]}/>
+//         <ResCard resData={resData[5]}/>
+//         <ResCard resData={resData[1]}/>
+//         <ResCard resData={resData[0]}/>
+//         <ResCard resData={resData[4]}/>
+        
+//     </div>
+//   )
+
   return (
     <div className='flex flex-wrap justify-between'>
 
-        <ResCard resData={resData[5]}/>
-        <ResCard resData={resData[1]}/>
-        <ResCard resData={resData[0]}/>
-        <ResCard resData={resData[4]}/>
-        <ResCard resData={resData[5]}/>
-        <ResCard resData={resData[1]}/>
-        <ResCard resData={resData[0]}/>
-        <ResCard resData={resData[4]}/>
+        {resData.map((restaurant,index) => {
+            return <ResCard key = {resData[index].info.id} resData={restaurant}/>
+        })}
         
+        {/* {resData.map((restaurant,index) => <ResCard key = {resData[index].info.id} resData={restaurant}/>)} */}
+
     </div>
   )
 }
