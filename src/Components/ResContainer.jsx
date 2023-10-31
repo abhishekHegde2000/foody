74168104ae93f4f0794a8d8af4dc34b06d184970
abhishek Hegde <ahegde2021@gmail.com>
2023-10-31 13:14:59 +0530

    commit - 8
    added filter  button.
    created a find data state variable - set that same as hotel data.
    this will get a copy data it will search from hoteldata ut hotel data will not get modified

--- src/Components/ResContainer.jsx
+++ src/Components/ResContainer.jsx
@@ -9,13 +9,18 @@ import Shimmer from './Shimmer'
 
 const ResContainer = () => {
 
-const [HotelData, setHotelData] = useState([]);
 
+
+const [HotelData, setHotelData] = useState([]);
+const [findData, setFindData] = useState([]);
+
+const [searchTerm, setSearchTerm] = useState('');
 const fetchdata = async ()=>{
     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
     const dataJson = await data.json();
     
     setHotelData(dataJson.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setFindData(dataJson.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
 }
 
     useEffect(() => {
@@ -28,8 +33,8 @@ const filterClick = () => {
     // Ensure that HotelData is an array before applying the filter
     if (Array.isArray(HotelData)) {
         const filteredData = HotelData.filter((restaurant) => restaurant.info.avgRating > 4);
-        setHotelData(filteredData);
-        console.log(filteredData);
+        setFindData(filteredData);
+        
     }
 }
 
@@ -40,7 +45,29 @@ return (
         ) : (
             <div>
                 <div className='flex'>
-                    <Search />
+                    <div className='flex'>
+                    <input 
+                    className=' border border-black m-2 p-2 mr-0 pr-0'
+                    type="text"
+                    value={searchTerm}
+                    placeholder='search here'
+                    onChange={(e) => {
+                        setSearchTerm(e.target.value);
+                    }
+                    }
+                    />
+                    <button 
+                    onClick={
+                        ()=>{
+                            const filteredSearchData= HotelData.filter((restaurant) => restaurant.info.name.toLowerCase().includes(searchTerm.toLowerCase()));
+                            setFindData(filteredSearchData);
+                        }
+                        
+                    }
+                    className='m-2 p-2 ml-0 pl-0 border border-black border-solid border-gray-50'>Search</button>
+
+                    </div>
+
                     <button
                         onClick={filterClick}
                         className="cursor-pointer border border-black p-2 m-2 font-bold rounded"
@@ -49,7 +76,7 @@ return (
                     </button>
                 </div>
                 <div className='flex flex-wrap justify-between'>
-                    {HotelData.map((restaurant,index) => {
+                    {findData.map((restaurant,index) => {
                         return <ResCard key = {resData[index].info.id} resData={restaurant}/>
                     })}
                     {/* {resData.map((restaurant,index) => <ResCard key = {resData[index].info.id} resData={restaurant}/>)} */}
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 const Search = () => {
   return (
     <div>
-        <input 
-        className='border border-black m-2 p-2'
-        type="text"
-        placeholder='search here' />
+
 
     </div>
   )
