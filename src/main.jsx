ed8243c74d65607c5f9b2ce26e7a3e7ece5c78c8
abhishek Hegde <ahegde2021@gmail.com>
2023-10-31 18:55:44 +0530

    commit - 9
    Router added for about and contact
    app.js deleted and only main.jsx retained

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Header from './Components/Header.jsx'
+import ResContainer from './Components/ResContainer.jsx'
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import About from './Components/About.jsx'
+import Contact from './Components/Contact.jsx'
+
+
+const App =()=>{
+    return (
+        <div>
+          <Header />
+          <ResContainer />
+        </div>
+      )
+}
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+        path: "/about",
+        element: <About />,
+    },
+    {
+        path: "/contact",
+        element: <Contact />,
+    },
+  ]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+        <RouterProvider router={router} />
 )
